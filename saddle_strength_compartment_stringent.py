e51db2c56da35380d1af933b97399937d8fda1af
Priyojit Das <priyodas3@gmail.com>
2021-12-11 19:23:40 -0500

    Add files via upload

--- saddle_strength_compartment_stringent.py
+++ saddle_strength_compartment_stringent.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter
 from pylab import rcParams
-plt.rcParams.update({'font.size': 25})
+plt.rcParams.update({'font.size': 15})
 rcParams['figure.figsize'] = 20, 10
 import sys
 
@@ -38,8 +38,8 @@ for ch in chrm:
 	indv_strn = []
 	for cindx,cell in enumerate(cell_list):
 		zdf,indx = input_nd_process_hic(bases[cindx]+str(ch)+".zScore.matrix.gz")
-		dcomp = np.nan_to_num(np.array(pd.read_csv(bases[cindx]+str(ch)+".zScore.eigen1.bedGraph",header=None,sep='\t',comment='t')[3].astype('float')))
-		
+                dcomp = np.nan_to_num(np.array(pd.read_csv(bases[cindx]+str(ch)+".zScore.eigen1.bedGraph",header=None,sep='\t',comment='t')[3].astype('float')))
+
 		dcomp_zero = np.where(dcomp == 0)[0]
 
 		dcomp_filtered = np.delete(dcomp,dcomp_zero)
@@ -58,20 +58,13 @@ for ch in chrm:
 
 		zdf_filtered_sorted = zdf_filtered_sorted[:,dcomp_filtered_sorted]
 
-		dcomp_filtered_sorted_binary = dcomp_filtered[dcomp_filtered_sorted]
-
-		dcomp_filtered_sorted_binary[dcomp_filtered[dcomp_filtered_sorted] > 0] = 1
-		dcomp_filtered_sorted_binary[dcomp_filtered[dcomp_filtered_sorted] <= 0] = 3
+		dcomp_filtered_sorted_val = dcomp_filtered[dcomp_filtered_sorted]
 
 		#plt.plot(dcomp_filtered_sorted_binary)
 		#plt.show()
 
 		#sns.heatmap(np.dot(dcomp_filtered_sorted_binary.reshape((dcomp_filtered_sorted.shape[0],1)),dcomp_filtered_sorted_binary.reshape((1,dcomp_filtered_sorted.shape[0]))),cmap='seismic')
 		#plt.show()
-		
-		comp_dot = np.dot(dcomp_filtered_sorted_binary.reshape((dcomp_filtered_sorted.shape[0],1)),dcomp_filtered_sorted_binary.reshape((1,dcomp_filtered_sorted.shape[0])))
-
-		comp_dot = np.array(comp_dot,dtype='int32')
 
 		#sns.heatmap(zdf_filtered_sorted,cmap='seismic')
 		#plt.show()
@@ -81,12 +74,10 @@ for ch in chrm:
 
 		for i in xrange(0,zdf_filtered_sorted.shape[0],2):
 			tmpLst1 = []
-			tmpLst2 = []
+			dcomp_filtered_sorted_cg += [np.mean(dcomp_filtered_sorted_val[i:i+2])]
 			for j in xrange(0,zdf_filtered_sorted.shape[0],2):
 				tmpLst1.append(np.mean(zdf_filtered_sorted[i:i+2,j:j+2]))
-				tmpLst2.append(np.bincount(comp_dot[i:i+2,j:j+2].flatten()).argmax())
 			zdf_filtered_sorted_cg.append(tmpLst1)
-			dcomp_filtered_sorted_cg.append(tmpLst2)
 
 		zdf_filtered_sorted_cg = np.array(zdf_filtered_sorted_cg)
 
@@ -96,59 +87,58 @@ for ch in chrm:
 
 		uniq_dcomp = list(set(dcomp_filtered_sorted_cg.flatten()))
 
-		#print ch,cell,uniq_dcomp
-
 		#print dcomp_filtered_sorted_cg
 
 		#ns.heatmap(dcomp_filtered_sorted_cg,cmap='seismic',center=0)
 		#plt.show()
-	
-		comp_BB = zdf_filtered_sorted_cg[np.triu_indices(zdf_filtered_sorted_cg.shape[0])]
-		comp_BB = comp_BB[dcomp_filtered_sorted_cg[np.triu_indices(dcomp_filtered_sorted_cg.shape[0])] == uniq_dcomp[0]]
-		#comp_BB = zdf_filtered_sorted_cg[dcomp_filtered_sorted_cg == uniq_dcomp[0]]
-		comp_tBB = np.median(comp_BB[comp_BB > np.percentile(comp_BB,80)])
 
-		comp_BA = zdf_filtered_sorted_cg[np.triu_indices(zdf_filtered_sorted_cg.shape[0])]
-		comp_BA = comp_BA[dcomp_filtered_sorted_cg[np.triu_indices(dcomp_filtered_sorted_cg.shape[0])] == uniq_dcomp[1]]
-		#comp_tBA = np.median(comp_BA[comp_BA < np.percentile(comp_BA,20)])
-		comp_tBA = np.median(comp_BA[comp_BA > np.percentile(comp_BA,80)])
+		comp_BB = zdf_filtered_sorted_cg[dcomp_filtered_sorted_cg < np.percentile(dcomp_filtered_sorted_cg[dcomp_filtered_sorted_cg < 0],20),:]
+		comp_BB = comp_BB[:,dcomp_filtered_sorted_cg < np.percentile(dcomp_filtered_sorted_cg[dcomp_filtered_sorted_cg < 0],20)]
+		comp_tBB = np.median(comp_BB)
 
-		comp_AA = zdf_filtered_sorted_cg[np.triu_indices(zdf_filtered_sorted_cg.shape[0])]
-                comp_AA = comp_AA[dcomp_filtered_sorted_cg[np.triu_indices(dcomp_filtered_sorted_cg.shape[0])] == uniq_dcomp[2]]
-		#comp_AA = zdf_filtered_sorted_cg[dcomp_filtered_sorted_cg == uniq_dcomp[2]]
-		comp_tAA = np.median(comp_AA[comp_AA > np.percentile(comp_AA,80)])
+		comp_AA = zdf_filtered_sorted_cg[dcomp_filtered_sorted_cg > np.percentile(dcomp_filtered_sorted_cg[dcomp_filtered_sorted_cg > 0],80),:]
+		comp_AA = comp_AA[:,dcomp_filtered_sorted_cg > np.percentile(dcomp_filtered_sorted_cg[dcomp_filtered_sorted_cg > 0],80)]
+		comp_tAA = np.median(comp_AA)
 
-		#print cell,(comp_tAA + comp_tBB) / (2 * comp_tBA)
+		comp_BA = zdf_filtered_sorted_cg[dcomp_filtered_sorted_cg < np.percentile(dcomp_filtered_sorted_cg[dcomp_filtered_sorted_cg < 0],20),:]
+		comp_BA = comp_BA[:,dcomp_filtered_sorted_cg > np.percentile(dcomp_filtered_sorted_cg[dcomp_filtered_sorted_cg > 0],80)]
+		comp_tBA = np.median(comp_BA)
 
-		#comp_strn.append((comp_tAA + comp_tBB) / (2 * comp_tBA))
-		comp_strn.append((comp_tAA + comp_tBB) - (2 * comp_tBA))
-		indv_strn.append([comp_tBB-comp_tBA,comp_tAA-comp_tBA])
+		comp_AB = zdf_filtered_sorted_cg[dcomp_filtered_sorted_cg > np.percentile(dcomp_filtered_sorted_cg[dcomp_filtered_sorted_cg > 0],80),:]
+		comp_AB = comp_AB[:,dcomp_filtered_sorted_cg < np.percentile(dcomp_filtered_sorted_cg[dcomp_filtered_sorted_cg < 0],20)]
+		comp_tAB = np.median(comp_AB)
+
+		#print comp_tAA, comp_tBB, comp_tBA
+
+		comp_strn.append((comp_tAA + comp_tBB) - (comp_tBA + comp_tAB))
+		indv_strn.append([comp_tBB-comp_tBA,comp_tAA-comp_tAB])
 
 		#sns.heatmap(zdf_filtered_sorted_cg,cmap='seismic',center=0)
 		#plt.show()
-
-		indx_filtered = np.delete(indx,dcomp_zero)[:zdf_filtered_sorted_cg.shape[0]]
 
 		#sns.heatmap(zdf_filtered_sorted_cg,cmap='seismic',center=0)
 		#plt.show()
 
 		#zmean = zdf_filtered_sorted_cg.mean(axis=1)
 		#zval_arr.append(list(zmean))
-		
-		#plt.bar([cindx*2,cindx*2+1],[comp_tBB-comp_tBA,comp_tAA-comp_tBA],facecolor=color[cindx],label=cell_list[cindx],width = 0.4,edgecolor='black')
+		#print cell,comp_tBB,comp_tBA,comp_tAA,comp_tAB
+	#if ax1 is None:
+	#	fig, ax1 = plt.subplots()
+	#ax1.bar([cindx*2,cindx*2+1],[comp_tBB-comp_tBA,comp_tAA-comp_tBA],facecolor=color[cindx],label=cell_list[cindx],width = 0.4,edgecolor='black')
 	pos.append(list(np.argsort(comp_strn)))
 	strength.append(list(comp_strn))
 	istrength.append(indv_strn)
 	#plt.plot([_*2+0.5 for _ in xrange(len(cell_list))],comp_strn,linestyle='--',color='black',linewidth=3,marker='X')
-	#plt.xticks([_*2+0.5 for _ in xrange(len(cell_list))],scell_list)
-	#plt.ylabel('Rank')
-	#plt.savefig('Saddle1Cohort/chr'+str(ch)+'.png',dpi=300)
+	#plt.xticks([_*2+0.5 for _ in xrange(len(cell_list))],['MCF10A','MDAB','IMR90','STL001','BJ','MDAT','MDAC','CC2551','T47D','H460','ACHN','A549'])
+	#plt.ylabel('Avg Compartment Strength')
+	#plt.savefig('Saddle2Cohort/chr'+str(ch)+'.png',dpi=300)
 	#plt.clf()
 	#plt.close()
 
 pos = np.array(pos)
 strength = np.array(strength)
 istrength = np.array(istrength)
+
 print strength
 print istrength
 
@@ -164,6 +154,6 @@ for cindx,cell in enumerate(cell_list):
 	ax2.set_ylim([1,8])
 	ax2.set_ylabel('Average Compartment Strength')
 	#ax2.legend(loc=1)
-	plt.savefig(cell_list[cindx]+'_'+reps[cindx]+'.png',dpi=300,bbox_inches='tight', pad_inches=0)
+	plt.savefig(cell_list[cindx]+'_'+reps[cindx]+'_stringent.png',dpi=300,bbox_inches='tight', pad_inches=0)
 	plt.clf()
 	plt.close()
